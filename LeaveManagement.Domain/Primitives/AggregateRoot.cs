e64741e442a2d19f9405dc7cb1ce7bc19b16ef7e
Jaime <jaimelabana@gmail.com>
2026-01-19 19:27:52 +0800

    Started Domain Events

--- LeaveManagement.Domain/Primitives/AggregateRoot.cs
+++ LeaveManagement.Domain/Primitives/AggregateRoot.cs
@@ -3,8 +3,17 @@ namespace LeaveManagement.Domain.Primitives
 {
     public abstract class AggregateRoot : Entity
     {
+        private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
         protected AggregateRoot(Guid id) : base(id){}
-
         protected AggregateRoot() { }
+
+        protected void RaiseDomainEvent(IDomainEvent domainEvent)
+        {
+            _events.Add(domainEvent);
+        }
+        protected void ClearDomainEvents()
+        {
+            _events.Clear();
+        }
     }
 }
---
+++ LeaveManagement.Domain/Primitives/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeaveManagement.Domain.Primitives
+{
+    public interface IDomainEvent
+    {
+        DateTime OccuredON { get; }
+    }
+}
