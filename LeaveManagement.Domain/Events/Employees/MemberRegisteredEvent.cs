2b35824db35580b30788e4ca2d0c757551637414
Jaime <jaimelabana@gmail.com>
2026-01-20 23:30:42 +0800

    Continuing Domain Events

---
+++ LeaveManagement.Domain/Events/Employees/MemberRegisteredEvent.cs
@@ -0,0 +1,9 @@
+﻿using LeaveManagement.Application.DomainEvents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeaveManagement.Domain.Events.Employees
+{
+    public record MemberRegisteredEvent() : IDomainEvent;
+}
---
+++ SharedKernel/DomainEvents/IDomainEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagement.Application.DomainEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel.DomainEvents
+{
+    public interface IDomainEventHandler<in T>
+      where T : IDomainEvent
+    {
+        Task Handle(T domainEvent, CancellationToken ct = default);
+    }
+}
