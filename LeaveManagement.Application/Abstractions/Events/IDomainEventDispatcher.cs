72096c7b34528fa5b4295c8541a45c60eab9ffe7
Jaime <jaimelabana@gmail.com>
2026-01-24 23:45:29 +0800

    made progress in domain events

---
+++ LeaveManagement.Application/Abstractions/Events/IDomainEventDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using LeaveManagement.Application.DomainEvents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeaveManagement.Application.Abstractions.Events
+{
+    public interface IDomainEventDispatcher
+    {
+        Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken ct);
+    }
+}
---
+++ LeaveManagement.Infrastracture/Events/DomainEventsDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using LeaveManagement.Application.Abstractions.Events;
+using LeaveManagement.Application.DomainEvents;
+using Microsoft.Extensions.DependencyInjection;
+using SharedKernel.DomainEvents;
+
+namespace LeaveManagement.Infrastructure.Events
+{
+    public class DomainEventsDispatcher(IServiceProvider sp) : IDomainEventDispatcher
+    {
+        private readonly IServiceProvider _sp = sp;
+        public async Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken ct)
+        {
+            foreach (var domainEvent in domainEvents)
+            {
+                using IServiceScope scope = _sp.CreateScope();
+                var handlerType = typeof(IDomainEventHandler<>).MakeGenericType(domainEvent.GetType());
+                IEnumerable<object?> handlers = scope.ServiceProvider.GetServices(handlerType);
+
+                foreach(dynamic? handler in handlers)
+                {
+                    if (handler is null)
+                        continue;
+
+
+                }
+            }
+        }
+    }
+}
