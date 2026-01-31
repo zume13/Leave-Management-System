154b1126c16ce5661e7cbdd26d740391a9ad011c
Jaime <jaimelabana@gmail.com>
2026-01-31 22:32:03 +0800

    Tried to implement decorators

--- LeaveManagement.Application/Abstractions/Behaviors/IPipelineBehavior.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LeaveManagement.Application.Abstractions.Behaviors
-{
-    public interface IPipelineBehavior<Trequest, TResponse>
-    {
-        Task<TResponse> Handle(Trequest request, Func<Task<TResponse>> next, CancellationToken ct = default);
-    }
-}
---
+++ LeaveManagement.Application/Abstractions/Behaviors/LoggingDecorator.cs
@@ -0,0 +1,20 @@
+﻿using LeaveManagement.Application.Abstractions.Messaging;
+using Serilog;
+using SharedKernel.Shared.Result;
+
+namespace LeaveManagement.Application.Abstractions.Behaviors
+{
+    public class LoggingDecorator
+    {
+
+        internal sealed class CommandHandler<TCommand, TResponse>(
+            ICommandHandler<TCommand, TResponse> innerHandler,
+            ILogger<CommandHandler<TCommand, TResponse>> logger) : ICommandHandler<TCommand, TResponse> where TCommand : ICommand
+        {
+            public Task<ResultT<TResponse>> Handle(TCommand command, CancellationToken token = default)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
